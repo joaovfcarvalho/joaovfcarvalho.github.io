90500a330307e2de1485edcb607cc49649bef690
Joao Carvalho <joaovfernandescarvalho@gmail.com>
2024-12-26 19:50:15 -0300

    Fix setup.sh and create index.html

--- setup.sh
+++ setup.sh
@@ -2,12 +2,9 @@
 mkdir -p ~/.streamlit
 
 echo "[general]" > ~/.streamlit/credentials.toml
-echo "email = \"jogodoou99999@gmail.com\"" >> ~/.streamlit/credentials.toml
+echo "email = \"joaovfernandescarvalho@gmail.com\"" >> ~/.streamlit/credentials.toml
 
 echo "[server]" >> ~/.streamlit/config.toml
 echo "headless = true" >> ~/.streamlit/config.toml
 echo "enableCORS = false" >> ~/.streamlit/config.toml
 echo "port = \$PORT" >> ~/.streamlit/config.toml
-
-# Copie o arquivo de exemplos para o diretório onde o Streamlit está sendo executado
-cp exemplos.txt .
